fe3a56850ffbb4965181f34fe7e2821f955708a5
Vanessa Baldin <vanbaldin08@gmail.com>
2025-04-15 09:59:23 -0300

    resolução do exercicio02

--- index.php
+++ index.php
@@ -3,22 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01</title>
+    <title>Exercício 02</title>
 </head>
 <body>
-    <h1>Exercício 01</h1>
+    <h1>Exercício 02</h1>
     <hr>
     <p><i>Faça as chamadas e testes nesta página</i></p>
 
 <?php
 require_once "src/Livro.php";
 
-$livroA = new Livro();
+$livroA = new Livro("O Alquimista", "Paulo Coelho", 180);
 
-
-$livroA->setTitulo("O Alquimista");
-$livroA->setAutor("Paulo Coelho");
-$livroA->setPaginas(180);
 
 ?>
 
